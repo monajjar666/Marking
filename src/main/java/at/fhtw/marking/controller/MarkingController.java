aa1dac7de7d4145007ddb7c967a970aac484e30d
Mo <ic21b001@technikum-wien.at>
2022-12-21 18:24:19 +0100

    business logic commit

--- src/main/java/at/fhtw/marking/controller/MarkingController.java
+++ src/main/java/at/fhtw/marking/controller/MarkingController.java
@@ -1,4 +1,25 @@
 package at.fhtw.marking.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
 public class MarkingController {
+
+
+    public MarkingController() throws IOException {
+    }
+    @GetMapping("/api/mark")
+    public int calculateMark(@RequestParam int percentage ){
+    return switch (percentage){
+        case 90 -> 1;
+        case 50 -> 4;
+        case 0 -> 5;
+        default -> throw new IllegalStateException("Unexpected value: " + percentage);
+    };
+    }
+
 }
